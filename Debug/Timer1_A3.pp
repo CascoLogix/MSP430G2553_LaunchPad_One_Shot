2cf33fb90f36b89beea526dd92529c0dca9fb11e
Clint Stevenson <clintastevenson@gmail.com>
2014-12-20 09:00:01 -0600

    First commit.  Code is working and operates as a one-shot timer, used here as an LED strobe.  This allows visibility of a LED blink triggered by a sub 10mS pin pulse without using delay cycles.

---
+++ Debug/Timer1_A3.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+Timer1_A3.obj: ../Timer1_A3.c
+Timer1_A3.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer1_A3.h
+Timer1_A3.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+Timer1_A3.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+Timer1_A3.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+Timer1_A3.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h
+Timer1_A3.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h
+Timer1_A3.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/callback.h
+
+../Timer1_A3.c: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer1_A3.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/callback.h: 
---
+++ Debug/main.pp
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h
+main.obj: c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h
+main.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer0_A3.h
+main.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/callback.h
+main.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer1_A3.h
+main.obj: C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/GPIO.h
+
+../main.c: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/msp430_4.3.5/include/stdint.h: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer0_A3.h: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/callback.h: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/Timer1_A3.h: 
+C:/CC1101_workspace/MSP430G2553_LaunchPad_One_Shot/GPIO.h: 
